702c0e90e500ddbed5a2c3e39772180c4b8b9bbb
神奈川県 <103529757+kaaanagawa@users.noreply.github.com>
2022-06-25 15:32:31 +0200

    Update custom.js

--- spy-x-family/assets/js/custom.js
+++ spy-x-family/assets/js/custom.js
@@ -38,7 +38,7 @@ const episodes = {
   8: "https://link.eu1.storjshare.io/jvlkdhbkw4vzb2m6z4arzr7dgpzq/animes-others%2Fspy-x-family%2F8.mp4",
   9: "https://link.eu1.storjshare.io/jux6orlgo6osedqa5yxctqd4yzca/animes-others%2Fspy-x-family%2F9.mp4",
   10: "https://link.eu1.storjshare.io/jwmfc54vs7dwt7gy6x5ssyjcshwq/animes-others%2Fspy-x-family%2F10.mp4",
-  11: "",
+  11: "https://link.storjshare.io/jwwlesbw5tgmxtvvfahisrwbsesa/animes-others%2Fspy-x-family%2F11.mp4",
   12: "",
   13: "",
   14: "",
