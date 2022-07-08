378073fe4dab69a3c7ce32df9943b344abed7753
神奈川県 <103529757+kaaanagawa@users.noreply.github.com>
2022-07-08 23:19:51 +0200

    Update custom.js

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -19,22 +19,23 @@
 //   popup.style.transform = "translateX(-120%)";
 // });
 
-$(".watch")[0].innerHTML = "(Ep. " + localStorage.getItem('watchBla') + ")";
-$(".watch")[1].innerHTML = "(Ep. " + localStorage.getItem('watchBle') + ")";
-$(".watch")[2].innerHTML = "(Ep. " + localStorage.getItem('watchDem') + ")";
-$(".watch")[3].innerHTML = "(Ep. " + localStorage.getItem('watchDrS') + ")";
-$(".watch")[4].innerHTML = "(Ep. " + localStorage.getItem('watchHai') + ")";
-$(".watch")[5].innerHTML = "(Ep. " + localStorage.getItem('watchHor') + ")";
-$(".watch")[6].innerHTML = "(Ep. " + localStorage.getItem('watchHxH') + ")";
-$(".watch")[7].innerHTML = "(Ep. " + localStorage.getItem('watchIni') + ")";
-$(".watch")[8].innerHTML = "(Ep. " + localStorage.getItem('watchJJK') + ")";
-$(".watch")[9].innerHTML = "(Ep. " + localStorage.getItem('watchJBA') + ")";
-$(".watch")[10].innerHTML = "(Ep. " + localStorage.getItem('watchMHA') + ")";
-$(".watch")[11].innerHTML = "(Ep. " + localStorage.getItem('watchNar') + ")";
-$(".watch")[12].innerHTML = "(Ep. " + localStorage.getItem('watchOP') + ")";
-$(".watch")[13].innerHTML = "(Ep. " + localStorage.getItem('watchSNK') + ")";
-$(".watch")[14].innerHTML = "(Ep. " + localStorage.getItem('watchSxF') + ")";
-$(".watch")[15].innerHTML = "(Ep. " + localStorage.getItem('watchVin') + ")";
+$(".watch")[0].innerHTML = "(Ep. " + localStorage.getItem('watchAss') + ")";
+$(".watch")[1].innerHTML = "(Ep. " + localStorage.getItem('watchBla') + ")";
+$(".watch")[2].innerHTML = "(Ep. " + localStorage.getItem('watchBle') + ")";
+$(".watch")[3].innerHTML = "(Ep. " + localStorage.getItem('watchDem') + ")";
+$(".watch")[4].innerHTML = "(Ep. " + localStorage.getItem('watchDrS') + ")";
+$(".watch")[5].innerHTML = "(Ep. " + localStorage.getItem('watchHai') + ")";
+$(".watch")[6].innerHTML = "(Ep. " + localStorage.getItem('watchHor') + ")";
+$(".watch")[7].innerHTML = "(Ep. " + localStorage.getItem('watchHxH') + ")";
+$(".watch")[8].innerHTML = "(Ep. " + localStorage.getItem('watchIni') + ")";
+$(".watch")[9].innerHTML = "(Ep. " + localStorage.getItem('watchJJK') + ")";
+$(".watch")[10].innerHTML = "(Ep. " + localStorage.getItem('watchJBA') + ")";
+$(".watch")[11].innerHTML = "(Ep. " + localStorage.getItem('watchMHA') + ")";
+$(".watch")[12].innerHTML = "(Ep. " + localStorage.getItem('watchNar') + ")";
+$(".watch")[13].innerHTML = "(Ep. " + localStorage.getItem('watchOP') + ")";
+$(".watch")[14].innerHTML = "(Ep. " + localStorage.getItem('watchSNK') + ")";
+$(".watch")[15].innerHTML = "(Ep. " + localStorage.getItem('watchSxF') + ")";
+$(".watch")[16].innerHTML = "(Ep. " + localStorage.getItem('watchVin') + ")";
 
 for (const div of document.querySelectorAll('.watch')) {
   if (div.textContent.includes('(Ep. null)')) {
